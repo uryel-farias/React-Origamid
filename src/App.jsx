29e3b86454e7a9534234c0f32b7daf48bbb8caa1
Uryel Farias <uryel.farias@gmail.com>
2024-03-28 21:48:25 -0300

    Primeiro exercício sem ajuda.

--- src/App.jsx
+++ src/App.jsx
@@ -26,24 +26,9 @@ const mario = {
 const App = () => {
   const dados = mario;
 
-  const total = dados.compras
-    .map((item) => Number(item.preco.replace('R$ ', '')))
-    .reduce((a, b) => a + b);
-
-  return (
-    <div>
-      <p>Nome: {dados.cliente}</p>
-      <p>Idade: {dados.idade}</p>
-      <p>
-        Situação:{' '}
-        <span style={{ color: dados.ativa ? 'green' : 'red' }}>
-          {dados.ativa ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total: R$ {total}</p>
-      {total > 10000 && <p>Você está gastando muito</p>}
-    </div>
-  );
+  
+  return 
+   
 };
 
 export default App;
