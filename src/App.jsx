46890e2c08dab7e4cd97ffacd20e8af0818a0ccc
Uryel Farias <uryel.farias@gmail.com>
2024-04-04 23:22:48 -0300

    Exercício para praticar arrays em react.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
 import React from "react";
 
-const App = () => {
-  const livros = [
-    { nome: 'A Game of Thrones', ano: 1996 },
-    { nome: 'A Clash of Kings', ano: 1998 },
-    { nome: 'A Storm of Swords', ano: 2000 },
-  ];
+const produtos = [
+  {
+    id: 1,
+    nome: 'Smartphone',
+    preco: 'R$ 2000',
+    cores: ['#29d8d5', '#252a34', '#fc3766'],
+  },
+  {
+    id: 2,
+    nome: 'Notebook',
+    preco: 'R$ 3000',
+    cores: ['#ffd045', '#d4394b', '#f37c59'],
+  },
+  {
+    id: 3,
+    nome: 'Tablet',
+    preco: 'R$ 1500',
+    cores: ['#365069', '#47c1c8', '#f95786'],
+  },
+];
 
+const App = () => {
   return (
-    <ul>
-      {livros
-        .filter((livro) => livro.ano >= 1998)
-        .map((livro) => (
-          <li key={livro.nome}>
-            {livro.nome}, {livro.ano}
-          </li>
+    <section>
+      {produtos
+        .filter((produto) => Number(produto.preco.replace('R$ ', '')) > 1500)
+        .map((produto) => (
+          <div key={produto.id}>
+            <h1>{produto.nome}</h1>
+            <p>Preço: {produto.preco}</p>
+            <ul>
+              {produto.cores.map((cor) => (
+                <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>
+                  {cor}
+                </li>
+              ))}
+            </ul>
+          </div>
         ))}
-    </ul>
+    </section>
   );
 };
 
