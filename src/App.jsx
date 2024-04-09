9e1b21c5ade08682b38eec32c1ad2b0f75f0ac78
Uryel Farias <uryel.farias@gmail.com>
2024-04-09 00:54:20 -0300

    estudando eventos.

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
 import React from "react";
 
-const produtos = [
-  {
-    id: 1,
-    nome: 'Smartphone',
-    preco: 'R$ 2000',
-    cores: ['#29d8d5', '#252a34', '#fc3766'],
-  },
-  {
-    id: 2,
-    nome: 'Notebook',
-    preco: 'R$ 3000',
-    cores: ['#ffd045', '#d4394b', '#f37c59'],
-  },
-  {
-    id: 3,
-    nome: 'Tablet',
-    preco: 'R$ 1500',
-    cores: ['#365069', '#47c1c8', '#f95786'],
-  },
-];
-
 const App = () => {
+  function handleClick(event) {
+    alert('Comprou: ' + event.target.innerText);
+  }
+
   return (
-    <section>
-      {produtos
-        .filter((produto) => Number(produto.preco.replace('R$ ', '')) > 1500)
-        .map((produto) => (
-          <div key={produto.id}>
-            <h1>{produto.nome}</h1>
-            <p>Preço: {produto.preco}</p>
-            <ul>
-              {produto.cores.map((cor) => (
-                <li key={cor} style={{ backgroundColor: cor, color: 'white' }}>
-                  {cor}
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
-    </section>
+    <div>
+      <button onClick={handleClick}>Camisa</button>
+      <button onClick={handleClick}>Bermuda</button>
+    </div>
   );
 };
 
